9354b18ecf532deb5e9182a765dcdad37a3495e6
sergioahv1594 <sergio.herrera@blankfactor.com>
2021-11-03 16:32:00 -0600

    .gitignore file updated

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, SafeAreaView, StatusBar } from 'react-native';
+import MapView from './native/MapView';
+import RNCamera from './native/RNCamera';
+import RNShare from './native/RNShare';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      {/* <MapView style={{ flex: 1 }} /> */}
+      {/* <RNCamera style={{flex: 1}} /> */}
+      <Button
+        title="Share"
+        onPress={() => RNShare.open({ message: 'Message to share' })}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
